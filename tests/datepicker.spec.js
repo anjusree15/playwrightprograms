9a162810393031171a46b65ae00338c3d32028cd
anjusree15 <anjusree07@gmail.com>
2026-09-24 08:54:27 +0300

    commit

--- tests/datepicker.spec.js
+++ tests/datepicker.spec.js
@@ -1,10 +1,10 @@
-import test from "@playwright/test"
+import {test,expect} from "@playwright/test"
 test ("promptbox",async({page})=>{
 await page.goto("https://selenium.qabible.in/date-picker.php")
 await page.locator(".form-control.datepicker").click()
 await page.locator(".datepicker-days th.datepicker-switch").click()
 await page.locator(".datepicker-months th.datepicker-switch").click()
-const targetyear=2016
+const targetyear=2026
 const targetmonth=6
 const targetdate=12
 while(true)
@@ -32,5 +32,17 @@ while(true)
 //await page.getByText(targetyear.toString(),{exact:true}).click()
 await page.locator("span.year",{ hasText: targetyear.toString()}).click()
 await page.locator(".month").nth(targetmonth-1).click()
+await page.locator(".day",{ hasText: targetdate.toString()}).click()
+await page.locator("#button-one").click()
+    const msgShown=await page.locator("#message-one")
+    const msg=await msgShown.textContent()
+    console.log("Input",msg)
+    const inputBox=await page.locator(".form-control.datepicker")
+    const inputDate=await inputBox.textContent()
+    console.log(inputDate)
+    await expect(msg).toContain(inputDate)
+   // const inputlocator=await page.locator("#single-input-field").textContent()
+    //await expect (inputlocator).toHaveText()
+
 await page.waitForTimeout(3000)
 })
